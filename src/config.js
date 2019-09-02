48f5adbe8196f4cea67da7aa9d5197b46d0d671a
Fabian Lema <fabianlemac@gmail.com>
2019-09-02 15:17:16 -0500

    For migrate

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     TEST_DB_URL: process.env.TEST_DB_URL,
-    DB_URL: process.env.DB_URL,
+    DB_URL: process.env.DATABASE_URL || process.env.DB_URL,
     API_TOKEN: process.env.API_TOKEN
 }
